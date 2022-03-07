bc1ad10045e75b924d41e71f7562e3bd1358687c
Jessie <jessunix@gmail.com>
2022-03-07 12:12:07 +0800

    adjust hero h2 based on length

---
+++ assets/font.js
@@ -0,0 +1,33 @@
+function resizeFont() {
+  const heroHeadings = document.querySelectorAll('.hero-headline h2')
+
+  heroHeadings.forEach(el => {
+    if (el.style.fontSize) {
+      el.style.fontSize = ''
+    }
+    const maxHeight = el.offsetHeight
+    const text = el.firstChild
+    const height = text.offsetHeight
+    if (height > maxHeight) {
+      const size = parseInt(getComputedStyle(el).fontSize, 10)
+      const lineHeight = parseInt(getComputedStyle(el).lineHeight, 10) / size
+      const maxSize = maxHeight / lineHeight / 3
+      const minSize = maxHeight / height * size
+      let fontSize = minSize
+      el.style.fontSize = fontSize + 'px'
+      while(fontSize < maxSize) {
+        if (text.offsetHeight > maxHeight) {
+          fontSize = fontSize - 1
+          el.style.fontSize = fontSize + 'px'
+          break
+        }
+        fontSize += 1
+        el.style.fontSize = fontSize + 'px'
+      }
+    }
+  })
+}
+
+window.addEventListener('resize', resizeFont)
+window.addEventListener('orientationchange', resizeFont)
+resizeFont()
