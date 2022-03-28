6462cab064f4f2047ba2a4fd8db6ae063bb90fbb
Jessie <jessunix@gmail.com>
2022-03-28 14:12:32 +0800

    fix: slider scroll
    1. add scroll polyfill for IE and Safari
    2. hide slider control when slided to both ends

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
   const prevBtn = document.getElementById('prev')
   const nextBtn = document.getElementById('next')
   if (!sliderItems.length || !prevBtn || !nextBtn) return
-  if (sliderItems.length === 1) {
-    prevBtn.style.display = 'hidden'
-    nextBtn.style.display = 'hidden'
-  }
+  prevBtn.disabled = true
+  nextBtn.disabled = false
   const maxIndex = sliderItems.length - 1
+  let animationId = null
 
   prevBtn.addEventListener('click', () => {
     slide(slider, 'right')
@@ -18,19 +17,54 @@
     slide(slider, 'left')
   })
 
+
+
   function slide(slider, direction = 'left') {
-    index += direction === 'left' ? 1 : -1
-    if (index < 0) {
-      index = maxIndex
-    } else if (index > maxIndex) {
+    const _direction = direction === 'left' ? 1 : -1
+    index += _direction
+
+    prevBtn.disabled = false
+    nextBtn.disabled = false
+    if (index <= 0) {
       index = 0
+      prevBtn.disabled = !prevBtn.disabled
+    } else if (index >= maxIndex) {
+      index = maxIndex
+      nextBtn.disabled = !nextBtn.disabled
     }
     const offset = slider.getBoundingClientRect().width * index
-    if (slider.scrollTo) {
+
+    // IE doesn't have `scrollTo`
+    // while Safari doesn't support defining scroll behavior
+    if (slider.scrollTo && 'scrollBehavior' in slider.style) {
       slider.scrollTo({ left: offset, behavior: 'smooth' })
     } else {
-      // IE polyfill
-      slider.scrollLeft = offset
+      let start
+      const duration = 1000
+      function step(timestamp) {
+        if (!start) {
+          start = timestamp
+        }
+        const time = timestamp - start
+        // speed decreases as the node gets closer to the end
+        const speed = Math.max(Math.abs((offset - slider.scrollLeft) / duration), 1 / duration)
+        const distance = time * speed * _direction
+
+        // when distance is within acceptable range, stop animation
+        if (speed <= 1 / duration || time > duration) {
+          start = 0
+          slider.scrollLeft = offset
+          animationId = null
+        } else {
+          slider.scrollLeft = slider.scrollLeft + distance
+          animationId = requestAnimationFrame(step)
+        }
+      }
+
+      if (animationId) {
+        window.cancelAnimationFrame(animationId)
+      }
+      animationId = window.requestAnimationFrame(step)
     }
   }
 })(document.querySelector('.featured-inner'))
