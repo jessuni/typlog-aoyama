bcbe5c49b5396aa257fbb391771c475c3d25a66c
Jessie <jessunix@gmail.com>
2022-04-03 15:10:25 +0800

    fix: slider
    1. fix safari scroll snap mandatory conflict with programmatically
    setting scrollLeft
    2. observe scroll event to set slider control state

--- index.js
+++ index.js
@@ -14,9 +14,8 @@
   const prevBtn = document.getElementById('prev')
   const nextBtn = document.getElementById('next')
   if (!sliderItems.length || !prevBtn || !nextBtn) return
-  prevBtn.disabled = true
-  nextBtn.disabled = false
   const maxIndex = sliderItems.length - 1
+  setSliderControl(index)
   let animationId = null
 
   prevBtn.addEventListener('click', () => {
@@ -26,19 +25,28 @@
     slide(slider, 'left')
   })
 
-  function slide(slider, direction = 'left') {
-    const _direction = direction === 'left' ? 1 : -1
-    index += _direction
+  slider.addEventListener('scroll', () => {
+    index = Math.round(slider.scrollLeft / slider.getBoundingClientRect().width)
+    setSliderControl(index)
+  })
 
+  function setSliderControl(index) {
     prevBtn.disabled = false
     nextBtn.disabled = false
     if (index <= 0) {
       index = 0
-      prevBtn.disabled = !prevBtn.disabled
+      prevBtn.disabled = true
     } else if (index >= maxIndex) {
       index = maxIndex
-      nextBtn.disabled = !nextBtn.disabled
+      nextBtn.disabled = true
     }
+  }
+
+  function slide(slider, direction = 'left') {
+    const _direction = direction === 'left' ? 1 : -1
+    index += _direction
+
+    // setSliderControl(index)
     const offset = slider.getBoundingClientRect().width * index
 
     // IE doesn't have `scrollTo`
@@ -46,6 +54,9 @@
     if (slider.scrollTo && 'scrollBehavior' in slider.style) {
       slider.scrollTo({ left: offset, behavior: 'smooth' })
     } else {
+      const isSafari = Boolean(slider.scrollTo)
+      // mandatory scroll snap will ignore `scrollLeft`
+      slider.style.scrollSnapType = 'x proximity'
       let start
       const duration = 1000
       function step(timestamp) {
@@ -53,6 +64,7 @@
           start = timestamp
         }
         const time = timestamp - start
+
         // speed decreases as the node gets closer to the end
         const speed = Math.max(Math.abs((offset - slider.scrollLeft) / duration), 1 / duration)
         const distance = time * speed * _direction
@@ -60,14 +72,15 @@
         // when distance is within acceptable range, stop animation
         if (speed <= 1 / duration || time > duration) {
           start = 0
-          slider.scrollLeft = offset
+          isSafari ? slider.scroll({ left: offset }) : slider.scrollLeft = offset
+          slider.style.scrollSnapType = 'x mandatory'
           animationId = null
         } else {
-          slider.scrollLeft = slider.scrollLeft + distance
+          isSafari ? slider.scrollBy({ left: distance }) : slider.scrollLeft = slider.scrollLeft + distance
+
           animationId = requestAnimationFrame(step)
         }
       }
-
       if (animationId) {
         window.cancelAnimationFrame(animationId)
       }
